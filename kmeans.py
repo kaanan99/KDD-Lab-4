9fe1f2e7992008429a368cec80a186156c9a0147
Kaanan Kharwa <kaanan99@gmail.com>
2021-11-01 23:53:26 -0700

    Almost done with k means

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import argparse
+import math
 
 def get_data(csv_path):
    '''
@@ -19,6 +20,78 @@ def get_data(csv_path):
    D = D.iloc[1:, new_cols]
    return D
 
+def distance(x1, x2):
+   return (((x1-x2) **2) ** .5).sum()
+
+def find_farthest(D, centroids):
+   max_distance = -math.inf 
+   centroid = None
+   for point in D:
+      if np.any(point == centroids):
+         pass
+      else:
+         current_distance = 0
+         for c in centroids:
+            current_distance += distance(point, c)
+         if current_distance > max_distance:
+            max_distance = current_distance
+            centroid = point
+   return centroid
+
+
+def select_initial(D, k):
+   average = np.array([0.0] * D.shape[1])
+   for x in D:
+      average += x
+   average = average / len(D)
+   centroids = []
+   centroids.append(find_farthest(D, [average]))
+   for x in range(k-1):
+      new_centroid = find_farthest(D, [centroids])
+      centroids.append(new_centroid)
+   return centroids
+
+def calculate_SSE(centroids, assignments):
+   sse = 0
+   for x in range(len(centroids)):
+      for y in assignments[x]:
+         sse += distance(x, y)
+   return sse
+
+def check_stopping(centroids, assignments, prev_sse):
+   return abs(prev_sse - calculate_SSE(centroids, assignments))
+
+def assign_points(centroids, D):
+   assignments = [[] for x in range(len(centroids))]
+   for point in D:
+      min_distance = math.inf
+      index = None
+      for x in range(len(centroids)):
+         dis = distance(centroids[x], point)
+         if dis < min_distance:
+            min_distance = dis
+            index = x
+      assignments[index].append(point)
+   return assignments
+
+def find_new_centroids(assignments):
+   centroids = []
+   for cluster in assignments:
+      new_center = np.array([0.0] * len(cluster[0]))
+      for point in cluster:
+         new_center += point
+      new_center /= len(cluster)
+      centroids.append(new_center)
+   return centroids
+
+def print_output(centroids, assignments):
+   for x in range(len(centroids)):
+      print("Cluster " +  str(x) + ":")
+      print("Center:", centroids[x])
+      distances = [sum(y) for y in abs(assignments[x] - centroids[x])]
+      print("Max Dist. to Center:", max(distances))
+      print("Min Dist. to Center:", min(distances))
+      print("Avg Dist. to Center:", sum(distances) / len(distances))
 
 def main():
    parser = argparse.ArgumentParser("Run K Means Clustering on the dataset")
@@ -26,7 +99,20 @@ def main():
    parser.add_argument("-k", required=True, help = "The number of clusters")
    args = parser.parse_args()
    D = get_data(args.csv_path)
-   print(D)
+   D = D.to_numpy()
+   k = int(args.k)
+   thr = .1
+   init_centroids = select_initial(D, k)
+   assignments = assign_points(init_centroids, D)
+   prev_sse = calculate_SSE(init_centroids, assignments)
+   centroids = find_new_centroids(assignments)
+   assignments = assign_points(centroids, D)
+   while check_stopping(centroids, assignments, prev_sse) > thr:
+      centroids = find_new_centroids(assignments)
+      assignments = assign_points(centroids, D)
+      prev_sse = calculate_SSE(centroids, assignments)
+   print_output(centroids, assignments)
+
 
 if __name__ == '__main__':
    main()
