6abd5d6bbb8eafad925b2e3333879cb667954e27
jacob-gold <43824812+jacob-gold@users.noreply.github.com>
2021-11-02 16:33:20 -0700

    fixed implementation

--- dbscan.py
+++ dbscan.py
@@ -1,33 +1,82 @@
+import numpy as np
+import pandas as pd
+import from kmeans get_data
+
+class Point:
+
+    def __init__(self, value):
+        self.visited = False
+        self.label = None
+        self.neighborhood = []
+        self.core = None
+        self.value = value
+
+    def createNeighborhood (self, D, epsilon):
+        for x in D:
+            dist = distance(self.value, x)
+            if dist < epsilon:
+                self.neighborhood.append(x)
 
 
 def dbscan(argv):
     file = argv[1]
     epsilon = argv[2]
-    numPts = argv[3]
+    minPts = argv[3]
 
     coreNum = 0
-    D = []
-    for line in file:
-        for column in line:
-            for row in column:
-                D.append(row)
+    D = get_data(file)
+    D = D.to_numpy()
 
-    neighborhoods = {}
-    cores = {}
-    d_len = len(D)
-    for d in range (d_len):
-        distances = []
-        for i in range (D[d:d_len]):
-            dist = distance(d, i)
-            if dist <= epsilon:
-                distances.append(dist)
-        for i in range (D[d_len:d]):
-            dist = distance(d, i)
-            if dist <= epsilon:
-                distances.append(dist)
-        neighborhoods.append(d, distances)
-        if len(distances) >= epsilon:
-            cores.append(d, distances)
+    points = {}
+    cores = []
+    for x in D:
+        point = Point(x)
+        point.createNeighborhood(D, epsilon)
+        if len(point.neighborhood) >= minPts:
+            point.core = True
+            cores.append(x)
+        else:
+            point.core = False
+        points[x] = point
+
+    currentLabel = 0
+    clusters = {}
+
+    for x in cores:
+        point = points[x]
+        point.visited = True
+        if point.label == None:
+            currentLabel += 1
+            point.label = currentLabel
+            DensityConnected(point, currentLabel, points)
+
+
+    cluster_list = [[] * currentLabel]
+    noise = []
+    border = []
+
+    for x in points.keys():
+        point = points[x]
+        if point.visited == True:
+            if point.core == False:
+                border.append(x)
+            cluster_list[point.label-1].append(x)
+        else:
+            noise.append(x)
+    return cluster_list, cores, border, noise
+
+def DensityConnected(point, clusterId, points):
+
+    for x in point.neighborhood:
+        d = points[x]
+        d.label = clusterId
+        if d.visited == False:
+            d.visited = True
+            if d.core == True:
+                DensityConnected(d, clusterId, points)
+
+def distance(x1, x2):
+   return (((x1-x2) **2) ** .5).sum()
 
 
 if __name__ == '__main__':
