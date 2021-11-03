4c1e01ff740ef8c89fa3403c472a27e69962dcbe
Kaanan Kharwa <kaanan99@gmail.com>
2021-11-03 09:53:40 -0700

    Added classes and normalization options to kmeans

--- kmeans.py
+++ kmeans.py
@@ -3,22 +3,44 @@ import numpy as np
 import argparse
 import math
 
-def get_data(csv_path):
+def normalize(x, max_val, min_val):
+    '''
+    Parameters:
+       x - the value which is being normalized
+       max_val - the maximum value in the column
+       min_val - the minimum value in the column
+    Returns:
+       A numerical value which represents the normalized x
+    '''
+    return (x - min_val) / (max_val - min_val)
+
+def get_data(csv_path, col=None, norm = None):
    '''
    Parameters:
       csv_path - The path to the csv file
+      class_col - The name of the class column
    Returns:
       A pandas dataframe 
    '''
+   print(norm)
    D = pd.read_csv(csv_path) 
    restrictions = list(D.iloc[0:])
+   class_col = None
+   if col != None:
+      class_col = D.iloc[:, int(col)].to_list()
    cols = [x for x in range(D.shape[1])]
    new_cols = []
    for x in range(len(restrictions)):
       if int(float(restrictions[x])) == 1:
          new_cols.append(x)
    D = D.iloc[1:, new_cols]
-   return D
+   #Normalize Data
+   if norm != None and int(norm) == 1:
+      for x in range(D.shape[1]):
+         max_val = max(D.iloc[:, x])
+         min_val = min(D.iloc[:, x])
+         D.iloc[:, x] = D.iloc[:, x].apply(normalize, args=[max_val, min_val])
+   return D, class_col
 
 def distance(x1, x2):
    return (((x1-x2) **2) ** .5).sum()
@@ -50,12 +72,18 @@ def select_initial(D, k):
       new_centroid = find_farthest(D, [centroids])
       centroids.append(new_centroid)
    return centroids
+  
+
+def calc_SSE_one(center, assignments):
+   sse = 0
+   for x in assignments:
+      sse += distance(center, x)
+   return sse
 
 def calculate_SSE(centroids, assignments):
    sse = 0
    for x in range(len(centroids)):
-      for y in assignments[x]:
-         sse += distance(x, y)
+      sse += calc_SSE_one(centroids[x], assignments[x])
    return sse
 
 def check_stopping(centroids, assignments, prev_sse):
@@ -84,34 +112,57 @@ def find_new_centroids(assignments):
       centroids.append(new_center)
    return centroids
 
-def print_output(centroids, assignments):
+def print_output(centroids, assignments, classes, is_class):
    for x in range(len(centroids)):
-      print("Cluster " +  str(x) + ":")
+      print("\nCluster " +  str(x) + ":")
+      print("Cluster Size: " + str(len(assignments[x])))
       print("Center:", centroids[x])
       distances = [sum(y) for y in abs(assignments[x] - centroids[x])]
       print("Max Dist. to Center:", max(distances))
       print("Min Dist. to Center:", min(distances))
       print("Avg Dist. to Center:", sum(distances) / len(distances))
+      print("Sum of Squared Errors:", calc_SSE_one(centroids[x], assignments[x]))
+      print("Points in Cluster:")
+      for point in assignments[x]:
+         class_val = ""
+         if is_class != None:
+            class_val = classes[tuple(point)]
+         print(point, class_val)
 
 def main():
+   # Get Arguments
    parser = argparse.ArgumentParser("Run K Means Clustering on the dataset")
    parser.add_argument("csv_path", help = "The filepath to the dataset")
    parser.add_argument("-k", required=True, help = "The number of clusters")
+   parser.add_argument("-c", required=False, help = "The index of the class column")
+   parser.add_argument("-norm", required=False, help= "Set 1 to have to data normalized")
    args = parser.parse_args()
-   D = get_data(args.csv_path)
+   # Setting Everything Up 
+   print(args.norm)
+   D, class_col = get_data(args.csv_path, args.c, args.norm)
    D = D.to_numpy()
+   classes = {}
+   if class_col != None:
+      for x in range(len(D)):
+         classes[tuple(D[x])] = class_col[x]
    k = int(args.k)
    thr = .1
+   #Find Initial Centriods
    init_centroids = select_initial(D, k)
+   #Assign Points
    assignments = assign_points(init_centroids, D)
+   #Calculate SSE
    prev_sse = calculate_SSE(init_centroids, assignments)
+   #New Centroids and assignments
    centroids = find_new_centroids(assignments)
    assignments = assign_points(centroids, D)
+   #Calculate Centroids and Assign Points until Stopping condition
    while check_stopping(centroids, assignments, prev_sse) > thr:
       centroids = find_new_centroids(assignments)
       assignments = assign_points(centroids, D)
       prev_sse = calculate_SSE(centroids, assignments)
-   print_output(centroids, assignments)
+   #Print output
+   print_output(centroids, assignments, classes, args.c)
 
 
 if __name__ == '__main__':
